75fbdb4e6c380f7f6c89e6c62a0b35dd9e22ffa0
Sranav-stack <sranavb@gmail.com>
2026-07-25 17:31:09 +0530

    Remove zero division check from divide method
    Removed division by zero check in the divide method.

--- Hello.java
+++ Hello.java
@@ -10,9 +10,6 @@ public class Hello {
     }
 
     public static int divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
-        }
     
         return a / b;
     }
